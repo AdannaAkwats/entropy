44b1fff882e637791f338c5a04c6e5d5414cc2e9
Adanna <aa14415@ic.ac.uk>
2018-01-13 11:37:35 +0000

    Added functions and python windows executable - Von neumann function - Calculate random quantum state - Windows bat python execuatable

---
+++ build_entropy.bat
@@ -0,0 +1,3 @@
+rm -r dist
+rm -r build
+pyinstaller entropy.py
--- entropy.py
+++ entropy.py
@@ -8,8 +8,6 @@ def vonNeumann(A):
     # Get eigenvalues of A
     values, _ = LA.eig(A)
 
-    print(values)
-
     # Take the natural logarithm of the eigenvalues and sum them
     sum = 0
     for v in values:
@@ -17,7 +15,10 @@ def vonNeumann(A):
         if(v == 0) :
             l = 0
         else:
-            l = math.log(v)
+            try:
+                l = math.log(v)
+            except ValueError:
+                print "(The matrix is not a quantum state!)"
         sum += l
 
     return -sum
@@ -57,7 +58,7 @@ def generateRandomState(n):
     return A
 
 
-# Generate random unitary matrix
+# -- Generate random unitary matrix
 def Unitary(n):
 
     # generate a random complex matrix
@@ -103,7 +104,7 @@ def Unitary(n):
     return U, U_conj, I
 
 # Generate random A s.t A = UDU*, where D is diagonal, U is unitary matrix and
-# U* is conplex conjugate transpose of U 
+# U* is conplex conjugate transpose of U
 def generate(n):
 
     # Unitary matrix and its complex conjugate transpose
@@ -124,11 +125,86 @@ def generate(n):
 
     return A
 
+# Fill matrix with nunbers from standard input
+def fill_matrix_with_input(matrix, n):
+    rows = {}
+    row_num = 0
+    for line in fileinput.input():
+        rows[row_num] = line
+        row_num = row_num + 1
+        if(row_num == n) :
+            break
+    for i in range(n) :
+        each_row = rows[i].split(" ");
+        for j in range(n) :
+             matrix[i][j] = complex(each_row[j])
+
+# Intro questions
+def intro_input() :
+    print("Press N : Type new matrix")
+    print("Press G : Generate random matrix")
+    print("Press Q : Quit")
+    print(" ")
+    typ = raw_input("[N/G]: ")
+    print(" ")
+    return typ
+
+# Get dimension of mstrix from input
+def get_n_from_input():
+    n = input("What is the dimension (n) of the matrix? (eg. 3x3 matrix dimension = 3): ")
+    print(" ")
+    return n
+
+# Initialise matrix
+def init_matrix(n) :
+    temp = np.zeros((n,n))
+    matrix = np.array(temp, dtype=np.complex128)
+    return matrix
 
 # Testing...
 
-g = generate(2)
-print(g)
+# Read input
+import fileinput
+import sys
 
-v = vonNeumann(g)
-print(v)
+# Generating matrix to use
+print(" ")
+while True:
+    typ = intro_input()
+
+    # Choices
+    if typ in ['N', 'n', 'G', 'g'] :
+        n = get_n_from_input()
+        matrix = init_matrix(n)
+
+        if typ == 'N'or typ == 'n':
+            print "Input the %dx%d matrix, pressing enter after each row and having a space between each element." %(n,n,)
+            print(" ")
+            fill_matrix_with_input(matrix, n)
+        elif typ == 'G'or typ == 'g':
+            matrix = generate(n)
+        break
+    elif typ in ['Q', 'q']:
+        sys.exit()
+    else :
+        print "ERROR!: Input is not N, G or Q"
+        print(" ")
+
+print(" ")
+
+# Print original matrix to console
+print "Original Matrix: "
+print(" ")
+print matrix
+
+# For testing, remove warnings TODO
+import warnings
+warnings.filterwarnings("ignore")
+
+# Get and print Von Neumann value
+print(" ")
+print "Von Neumann transformation of Matrix: "
+print vonNeumann(matrix)
+
+# Pause at python exe
+raw_input()
